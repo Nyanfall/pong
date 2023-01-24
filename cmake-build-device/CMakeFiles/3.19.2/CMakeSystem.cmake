be43b64d7083036e773a3e0118fe05af81ce09b4
Artem Panchenko <meateatersco@gmail.com>
2023-01-24 02:05:58 +0200

    eblya with CMAKE

---
+++ cmake-build-device/CMakeFiles/3.19.2/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-6.1.7-zen1-1-zen")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "6.1.7-zen1-1-zen")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/nyanfall/.local/share/playdate-sdk/C_API/buildsupport/arm.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "ARM")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
