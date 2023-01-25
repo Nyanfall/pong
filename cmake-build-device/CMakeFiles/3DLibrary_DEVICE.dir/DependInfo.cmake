5df671dd5baf7dbcb98ed19f3d210b3bedbe1af4
Artem Panchenko <meateatersco@gmail.com>
2023-01-25 22:07:50 +0200

    first succsesful demo build ;3

---
+++ cmake-build-device/CMakeFiles/3DLibrary_DEVICE.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/nyanfall/.local/share/playdate-sdk/C_API/buildsupport/setup.c" "/home/nyanfall/projects/playdate/c-first-try/pong/cmake-build-device/CMakeFiles/3DLibrary_DEVICE.dir/home/nyanfall/.local/share/playdate-sdk/C_API/buildsupport/setup.c.obj"
+  "/home/nyanfall/projects/playdate/c-first-try/pong/luaglue.c" "/home/nyanfall/projects/playdate/c-first-try/pong/cmake-build-device/CMakeFiles/3DLibrary_DEVICE.dir/luaglue.c.obj"
+  "/home/nyanfall/projects/playdate/c-first-try/pong/main.c" "/home/nyanfall/projects/playdate/c-first-try/pong/cmake-build-device/CMakeFiles/3DLibrary_DEVICE.dir/main.c.obj"
+  "/home/nyanfall/projects/playdate/c-first-try/pong/mini3d/3dmath.c" "/home/nyanfall/projects/playdate/c-first-try/pong/cmake-build-device/CMakeFiles/3DLibrary_DEVICE.dir/mini3d/3dmath.c.obj"
+  "/home/nyanfall/projects/playdate/c-first-try/pong/mini3d/mini3d.c" "/home/nyanfall/projects/playdate/c-first-try/pong/cmake-build-device/CMakeFiles/3DLibrary_DEVICE.dir/mini3d/mini3d.c.obj"
+  "/home/nyanfall/projects/playdate/c-first-try/pong/mini3d/render.c" "/home/nyanfall/projects/playdate/c-first-try/pong/cmake-build-device/CMakeFiles/3DLibrary_DEVICE.dir/mini3d/render.c.obj"
+  "/home/nyanfall/projects/playdate/c-first-try/pong/mini3d/scene.c" "/home/nyanfall/projects/playdate/c-first-try/pong/cmake-build-device/CMakeFiles/3DLibrary_DEVICE.dir/mini3d/scene.c.obj"
+  "/home/nyanfall/projects/playdate/c-first-try/pong/mini3d/shape.c" "/home/nyanfall/projects/playdate/c-first-try/pong/cmake-build-device/CMakeFiles/3DLibrary_DEVICE.dir/mini3d/shape.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "TARGET_EXTENSION=1"
+  "TARGET_PLAYDATE=1"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../mini3d"
+  "/home/nyanfall/.local/share/playdate-sdk/C_API"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-device/CMakeFiles/3DLibrary_DEVICE.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "../3DLibrary.pdx"
+  "3DLibrary_DEVICE.elf"
+  "3DLibrary_DEVICE.elf.pdb"
+  "CMakeFiles/3DLibrary_DEVICE.dir/home/nyanfall/.local/share/playdate-sdk/C_API/buildsupport/setup.c.obj"
+  "CMakeFiles/3DLibrary_DEVICE.dir/luaglue.c.obj"
+  "CMakeFiles/3DLibrary_DEVICE.dir/main.c.obj"
+  "CMakeFiles/3DLibrary_DEVICE.dir/mini3d/3dmath.c.obj"
+  "CMakeFiles/3DLibrary_DEVICE.dir/mini3d/mini3d.c.obj"
+  "CMakeFiles/3DLibrary_DEVICE.dir/mini3d/render.c.obj"
+  "CMakeFiles/3DLibrary_DEVICE.dir/mini3d/scene.c.obj"
+  "CMakeFiles/3DLibrary_DEVICE.dir/mini3d/shape.c.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/3DLibrary_DEVICE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
